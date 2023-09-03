d45749101cb7ec27de0db3ffba356d3bc13683a0
jeff-0324 <jeffap324@gmail.com>
2023-09-04 00:30:05 +0900

    Initial Commit

---
+++ BMICalculatorPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculatorPractice
+//
+//  Created by jae hoon lee on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
